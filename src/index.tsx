1546dde0fd74fdceac6f1a2116e42ea2bc319dfc
Ryan Graff <ryantgraff@resistdesign.com>
2020-05-02 22:14:07 -0400

    chore: Add editor config file.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
+import React, {FC} from 'react';
+
 export default class App {
-    public doStuff(message: string = 'message'): void {
-        console.log(message);
-    }
+  public doStuff(message: string = 'message'): void {
+    console.log(message);
+  }
 }
+
+const Thing: FC = () => (
+  <div>
+    <h2>Thing</h2>
+  </div>
+);
