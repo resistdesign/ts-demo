cf590502b7e58fec0c62adeeac99bddc2d1cb9a3
Ryan Graff <ryantgraff@resistdesign.com>
2020-05-02 21:02:51 -0400

    test: (App) Add tests.

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,10 +1,20 @@
 import expect from 'expect.js';
+import sinon from 'sinon';
 import App from "./index";
 
 export default {
     App: {
         'should be a class': () => {
             expect(App).to.be.a(Function);
+        },
+        'should do stuff': () => {
+            const message = 'MESSAGE_TEXT';
+            const stub = sinon.stub(console, 'log');
+            const app = new App();
+
+            app.doStuff(message);
+
+            expect(stub.calledWith(message)).to.be(true);
         }
     }
 };
